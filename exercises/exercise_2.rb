0af154d58698e4c335a1f895a22ffdc4f9c5ffdd
sarahmanhard <smanhard88@gmail.com>
2023-10-19 17:32:01 -0700

    Initial commit

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#Load the first store with id = 1 and assign it to @store1
+@store1 =Store.find(1)
+
+@store2 =Store.find(2)
+
+# Update the first store (@store1) instance in the database
+@store1.update(name: 'Walmart')
